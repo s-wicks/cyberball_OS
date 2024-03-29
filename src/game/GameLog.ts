20d9d09b3d72ded403daa36411c9fd3128b91300
lamotnat <nathanl4school@gmail.com>
2024-03-29 18:23:52 -0400

    some more work and cleaning

--- src/game/GameLog.ts
+++ src/game/GameLog.ts
@@ -3,20 +3,28 @@ import CyberballGameController from "./CyberballGameController";
 export function addLogging(controller: CyberballGameController) {
     let gameLog = [];
 
-    // can elect to use timer/gameclock module here? have to notify game clock of every throw and it can report the waits
+    // TODO - note - while wait and waitTime have been added to the callback in CyberballGameController they are not implemented
     controller.throwBallCallbacks.addCallback("log throw", (thrower, reciever, waitTime) => {
         gameLog.push({ "type": "throw", "thrower": thrower, "reciever": reciever, "wait": waitTime });
     });
 
     controller.CPULeaveCallbacks.addCallback("log leave", (id, reason) => {
-        gameLog.push({ "type": "CPU leave", "leaver": id, "reason": reason, "time": this.cyberballGameController.reportTimeSinceStart() })
+        gameLog.push({ "type": "CPU leave", "leaver": id, "reason": reason, "time": controller.reportTimeSinceStart() });
     });
 
     controller.humanPlayerMayLeaveCallbacks.addCallback("log player may leave", reason => {
-        gameLog.push({ "type": "player may leave", "reason": reason, "time": this.cyberballGameController.reportTimeSinceStart() })
+        gameLog.push({ "type": "player may leave", "reason": reason, "time": controller.reportTimeSinceStart() });
     });
 
-    controller.gameEndCallbacks.addCallback("log game end", reason => {
-        gameLog.push({ "type": "game end", "reason": reason, "time": this.cyberballGameController.reportTimeSinceStart() })
+    controller.gameEndCallbacks.addCallback("log and postgame end", reason => {
+        gameLog.push({ "type": "game end", "reason": reason, "time": controller.reportTimeSinceStart() });
+
+        processAndReportGameLog(gameLog);
     });
+}
+
+function processAndReportGameLog(gameLog) {
+    // TODO - do postprocessing and format to be one big message
+
+    window.parent.postMessage(null /* TODO*/);
 }
