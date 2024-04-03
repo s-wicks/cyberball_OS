eddf67611ca5194c4bdbf0c8eba4128c6870ae51
lamotnat <nathanl4school@gmail.com>
2024-04-02 22:41:41 -0400

    should have post-processing implemented now

--- src/game/GameLog.ts
+++ src/game/GameLog.ts
@@ -1,11 +1,15 @@
 import CyberballGameController from "./CyberballGameController";
 
+let numPlayers = 0;
+
 export function addLogging(controller: CyberballGameController) {
     let gameLog = [];
 
     // TODO Nate - note - while wait and waitTime have been added to the callback in CyberballGameController they are not implemented
     controller.throwBallCallbacks.addCallback("log throw", (thrower, reciever, waitTime) => {
         gameLog.push({ "type": "throw", "thrower": thrower, "reciever": reciever, "wait": waitTime });
+
+        numPlayers = Math.max(numPlayers, thrower + 2, reciever + 2);
     });
 
     controller.CPULeaveCallbacks.addCallback("log leave", (id, reason) => {
@@ -16,7 +20,7 @@ export function addLogging(controller: CyberballGameController) {
         gameLog.push({ "type": "player may leave", "reason": reason, "time": controller.reportTimeSinceStart() });
     });
 
-    controller.gameEndCallbacks.addCallback("log and postgame end", reason => {
+    controller.gameEndCallbacks.addCallback("log and post game end", reason => {
         gameLog.push({ "type": "game end", "reason": reason, "time": controller.reportTimeSinceStart() });
 
         processAndReportGameLog(gameLog);
@@ -24,7 +28,49 @@ export function addLogging(controller: CyberballGameController) {
 }
 
 function processAndReportGameLog(gameLog) {
-    // TODO Nate - do postprocessing and format to be one big message
+    // Postprocessing
 
-    window.parent.postMessage(null /* TODO Nate */);
+    let throwStats = Array(numPlayers).fill(Array(numPlayers).fill(0));
+    let totalThrows = 0;
+
+    for (let entry of gameLog) {
+        if (entry.type === "throw") {
+            //account for numbering - user -1 is now index 0, etc
+            throwStats[entry.thrower + 1][entry.reciever + 1]++;
+            totalThrows++;
+        }
+    }
+
+    window.parent.postMessage(
+        {
+            "game_log": gameLog,
+            "throws_formatted": throwStats,
+            "total_throws": totalThrows,
+            "player_throws_list": buildListOfPlayerThrows(throwStats)
+        }
+    );
+}
+
+function buildListOfPlayerThrows(throwStats) {
+    let msg = {
+        Player_1_to_Player_2: 0,
+        Player_1_to_Player_3: 0,
+        Player_1_to_Player_4: 0,
+        Player_2_to_Player_1: 0,
+        Player_2_to_Player_3: 0,
+        Player_2_to_Player_4: 0,
+        Player_3_to_Player_1: 0,
+        Player_3_to_Player_2: 0,
+        Player_3_to_Player_4: 0,
+    };
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (i < numPlayers && j < numPlayers) {
+                msg['Player_' + (i + 1) + '_to_Player_' + (j + 1)] = throwStats[i][j];
+            }
+        }
+    }
+
+    return msg;
 }
