a1b32d418878d8e6f5e04f1f7933bd7bba8905e7
Drew Smith <smithdrew555@gmail.com>
2024-04-05 10:25:12 -0400

    update leave chance

--- src/game/LeaveTriggers.ts
+++ src/game/LeaveTriggers.ts
@@ -22,6 +22,11 @@ export function addPlayerMayLeaveTriggers(controller: CyberballGameController, s
     addCpuLeaveTriggers(controller, CyberballGameModel.humanPlayerId, callback, settings.player, settings.computerPlayers.length);
 }
 
+function shouldDisableLeaveTrigger(settings: CpuSettingsModel | PlayerSettingsModel, leavePercentageAttr: string): boolean {
+    const leavePercentage = leavePercentageAttr in settings ? settings[leavePercentageAttr] : 0;
+    return leavePercentage > Math.random() * 100;
+}
+
 export function addCpuLeaveTriggers(
     controller: CyberballGameController,
     playerId: number,
@@ -30,30 +35,29 @@ export function addCpuLeaveTriggers(
     totalNumberOfCpus: number,
 ) {
     if (settings.leaveTrigger & LeaveTrigger.Turn) {
+        if (shouldDisableLeaveTrigger(settings, 'leaveTurnChance')) return;
         let leaveTurn = settings.leaveTurn + (Math.random() * 2 - 1) * settings.leaveTurnVariance;
-        const leaveTurnChance = 'leaveTurnChance' in settings ? settings.leaveTurnChance : 100;
-        addCpuTurnLeaveTrigger(controller, playerId, leaveCallback, leaveTurn, leaveTurnChance);
+        addCpuTurnLeaveTrigger(controller, playerId, leaveCallback, leaveTurn);
     }
     if (settings.leaveTrigger & LeaveTrigger.Time) {
+        if (shouldDisableLeaveTrigger(settings, 'leaveTimeChance')) return;
         let leaveTimeSeconds = settings.leaveTime + (Math.random() * 2 - 1) * settings.leaveTimeVariance;
         let leaveTimeMilliseconds = leaveTimeSeconds * 1000;
-        let leaveTimeChance = 'leaveTimeChance' in settings ? settings.leaveTimeChance : 100;
-        addCpuTimeLeaveTrigger(controller, playerId, leaveCallback, leaveTimeMilliseconds, leaveTimeChance);
+        addCpuTimeLeaveTrigger(controller, playerId, leaveCallback, leaveTimeMilliseconds);
     }
     if (settings.leaveTrigger & LeaveTrigger.Ignored) {
+        if (shouldDisableLeaveTrigger(settings, 'leaveIgnoredChance')) return;
         let leaveThrows = settings.leaveIgnored + (Math.random() * 2 - 1) * settings.leaveIgnoredVariance;
-        let leaveIgnoredChance = 'leaveIgnoredChance' in settings ? settings.leaveIgnoredChance : 100;
-        addCpuIgnoredLeaveTrigger(controller, playerId, leaveCallback, leaveThrows, leaveIgnoredChance);
+        addCpuIgnoredLeaveTrigger(controller, playerId, leaveCallback, leaveThrows);
     }
     if (settings.leaveTrigger & LeaveTrigger.OtherLeaver) {
-        let leaveOtherLeaverChance = 'leaveOtherLeaverChance' in settings ? settings.leaveOtherLeaverChance : 100;
-        addCpuOtherLeaverLeaveTrigger(controller, playerId, leaveCallback, settings.leaveOtherLeaver, leaveOtherLeaverChance, totalNumberOfCpus);
+        if (shouldDisableLeaveTrigger(settings, 'leaveOtherLeaverChance')) return;
+        addCpuOtherLeaverLeaveTrigger(controller, playerId, leaveCallback, settings.leaveOtherLeaver, totalNumberOfCpus);
     }
     if (settings.leaveTrigger & LeaveTrigger.TimeIgnored) {
-        let leaveTimeSeconds = settings.leaveTimeIgnored + (Math.random() * 2 - 1) * settings.leaveTimeIgnored;
-        let leaveTimeMilliseconds = leaveTimeSeconds * 1000;
-        let leaveTimeIgnoredChance = 'leaveTimeIgnoredChance' in settings ? settings.leaveTimeIgnoredChance : 100;
-        addCpuTimeIgnoredLeaveTrigger(controller, playerId, leaveCallback, leaveTimeMilliseconds, leaveTimeIgnoredChance);
+        if (shouldDisableLeaveTrigger(settings, 'leaveTimeIgnoredChance')) return;
+        let leaveTimeMilliseconds = settings.leaveTimeIgnored + (Math.random() * 2 - 1) * settings.leaveTimeIgnored;
+        addCpuTimeIgnoredLeaveTrigger(controller, playerId, leaveCallback, leaveTimeMilliseconds);
     }
 }
 
@@ -62,11 +66,9 @@ export function addCpuTurnLeaveTrigger(
     playerId: number,
     leaveCallback: (reason: string) => void, 
     leaveTurn: number, 
-    leaveTurnChance: number
 ) {
     controller.catchBallCallbacks.addCallback(`Player ${playerId} LeaveTrigger.Turn`, () => {
-        let rnd = Math.random() * 100 <= leaveTurnChance;
-        if (controller.model.throwCount >= leaveTurn && rnd) {
+        if (controller.model.throwCount >= leaveTurn) {
             leaveCallback("throws elapsed");
         }
     });
@@ -77,18 +79,17 @@ export function addCpuTimeLeaveTrigger(
     playerId: number,
     leaveCallback: (reason: string) => void,
     leaveTime: number,
-    leaveTimeChance: number
 ) {
-    let rnd = Math.random() * 100 <= leaveTimeChance;
-    if (leaveTime > 0 && rnd) {
-        setTimeout(() => {
-            leaveCallback('time elapsed');
-            // need to add a callback just in case this cpu is currenty catching or holding the ball
-            controller.throwBallCallbacks.addCallback(`Player ${playerId} LeaveTrigger.Time`, () => {
-                leaveCallback('time elapsed');
-            });
-        }, leaveTime);
+    if (leaveTime <= 0) {
+        return;
     }
+    setTimeout(() => {
+        leaveCallback('time elapsed');
+        // need to add a callback just in case this cpu is currenty catching or holding the ball
+        controller.throwBallCallbacks.addCallback(`Player ${playerId} LeaveTrigger.Time`, () => {
+            leaveCallback('time elapsed');
+        });
+    }, leaveTime);
 }
 
 export function addCpuIgnoredLeaveTrigger(
@@ -96,7 +97,6 @@ export function addCpuIgnoredLeaveTrigger(
     playerId: number,
     leaveCallback: (reason: string) => void,
     leaveThrows: number,
-    leaveIgnoredChance: number
 ) {
     let throwsIgnored = 0;
     controller.catchBallCallbacks.addCallback(`Player ${playerId} LeaveTrigger.Ignored`, id => {
@@ -105,9 +105,8 @@ export function addCpuIgnoredLeaveTrigger(
         } else {
             throwsIgnored += 1;
         }
-        let rnd = Math.random() * 100 <= leaveIgnoredChance;
 
-        if (throwsIgnored >= leaveThrows && rnd) {
+        if (throwsIgnored > leaveThrows) {
             leaveCallback('throws ignored')
         }
     });
@@ -118,13 +117,11 @@ export function addCpuOtherLeaverLeaveTrigger(
     playerId: number,
     leaveCallback: (reason: string) => void,
     leaveOtherLeaver: number,
-    leaveOtherLeaverChance: number,
     totalNumberOfCpus: number
 ) {
     controller.CPULeaveCallbacks.addCallback(`Player ${playerId} LeaveTrigger.OtherLeaver`, () => {
-        let rnd = Math.random() * 100 <= leaveOtherLeaverChance;
         let enoughPlayersLeft = totalNumberOfCpus - controller.model.remainingCpuPlayerIds.size >= leaveOtherLeaver;
-        if (enoughPlayersLeft && rnd) {
+        if (enoughPlayersLeft) {
             leaveCallback('other leavers')
         }
     });
@@ -135,14 +132,10 @@ export function addCpuTimeIgnoredLeaveTrigger(
     playerId: number,
     leaveCallback: (reason: string) => void,
     leaveTime: number,
-    leaveTimeIgnoredChance: number
 ) {
     const callback = () => {
-        let rnd = Math.random() * 100 <= leaveTimeIgnoredChance;
-        if (rnd) {
-            // its ok if this fails because it means that they will be recieving the ball
-            leaveCallback('time ignored')
-        }
+        // its ok if this fails because it means that they will be recieving the ball
+        leaveCallback('time ignored')
     };
     let timeout = setTimeout(callback, leaveTime);
     controller.catchBallCallbacks.addCallback(`Player ${playerId} LeaveTrigger.TimeIgnored`, id => {
