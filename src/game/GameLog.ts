5daddddc2d47b5ffa691b67c82d0a9d76d9fe045
Drew Smith <smithdrew555@yahoo.com>
2024-04-08 21:31:16 -0400

    fix qualtrics reporting

--- src/game/GameLog.ts
+++ src/game/GameLog.ts
@@ -1,10 +1,15 @@
 import { SettingsModel } from "models/settings-model";
 import CyberballGameController from "./CyberballGameController";
 
-let numPlayers = 0;
-let gameLog = [];
-let timeAtCatch = 0;
-let humanLeaveReason = "";
+interface GameLog {
+    type: string, 
+    thrower?: number, 
+    reciever?: number, 
+    wait?: number, 
+    leaver?: number, 
+    reason?: string, 
+    time?: number
+}
 
 /**
  * NOTE!!!
@@ -15,6 +20,12 @@ let humanLeaveReason = "";
  * @param controller 
 */
 export function addGameLogging(controller: CyberballGameController, settings: SettingsModel) {
+    let numPlayers = settings.computerPlayers.length + 1;
+    let gameLog: Array<GameLog> = [];
+    let timeAtCatch = 0;
+    let humanLeaveReason = "";
+    let humanMayLeaveTime = 0;
+
     controller.catchBallCallbacks.addCallback("grab time at catch", () => {
         timeAtCatch = controller.reportTimeSinceStart();
     })
@@ -31,18 +42,32 @@ export function addGameLogging(controller: CyberballGameController, settings: Se
         gameLog.push({ "type": "player may leave", "reason": reason, "time": controller.reportTimeSinceStart() });
 
         humanLeaveReason = reason;
+        humanMayLeaveTime = controller.reportTimeSinceStart();
     });
 
     controller.gameEndCallbacks.addCallback("log and post game end", reason => {
         gameLog.push({ "type": "game end", "reason": reason, "time": controller.reportTimeSinceStart() });
 
-        numPlayers = settings.computerPlayers.length + 1;
-        processAndReportGameLog(controller.model.throwCount, controller.reportTimeSinceStart());
+        processAndReportGameLog(
+            controller.model.throwCount, 
+            controller.reportTimeSinceStart(),
+            numPlayers,
+            gameLog,
+            humanLeaveReason,
+            humanMayLeaveTime
+        );
     });
 }
 
-function processAndReportGameLog(throwCount, totalTime) {
-    let throwStats = Array(numPlayers).fill([]).map(() => Array(numPlayers).fill(0));
+function processAndReportGameLog(
+    throwCount: number,
+    totalTime: number, 
+    numPlayers: number,
+    gameLog: Array<GameLog>,
+    humanLeaveReason: string,
+    humanMayLeaveTime: number
+) {
+    let throwStats: Array<Array<number>> = Array(numPlayers).fill([]).map(() => Array(numPlayers).fill(0));
 
     for (let entry of gameLog) {
         if (entry.type === "throw") {
@@ -55,30 +80,21 @@ function processAndReportGameLog(throwCount, totalTime) {
     let msg = {
         "game_log": gameLog,
         "throws_formatted": throwStats,
-        "player_throws_list": buildListOfPlayerThrows(throwStats),
+        "player_throws_list": buildListOfPlayerThrows(throwStats, numPlayers),
         "total_throws": throwCount,
-        "player_may_leave": humanLeaveReason,
+        "player_may_leave_reason": humanLeaveReason,
+        "player_may_leave_time": humanMayLeaveTime,
         "total_time": totalTime
     };
     console.log(msg)
     //note: shouldn't have to - if debugging and need to see data from postMessage(), use
     // `window.addEventListener('message', (post_msg) => console.log(post_msg.data))` in browser console
     // (post_msg.data should 100% match the console.log above - resulting in two back to back identical messages)
-    window.postMessage(msg);
+    window.parent.postMessage(msg, '*');
 }
 
-function buildListOfPlayerThrows(throwStats) {
-    let msg = {
-        "Player_1_to_Player_2": 0,
-        "Player_1_to_Player_3": 0,
-        "Player_1_to_Player_4": 0,
-        "Player_2_to_Player_1": 0,
-        "Player_2_to_Player_3": 0,
-        "Player_2_to_Player_4": 0,
-        "Player_3_to_Player_1": 0,
-        "Player_3_to_Player_2": 0,
-        "Player_3_to_Player_4": 0,
-    };
+function buildListOfPlayerThrows(throwStats: Array<Array<number>>, numPlayers: number) {
+    let msg = {};
 
     let loopCount = Math.min(numPlayers, 4)
     for (let i = 0; i < loopCount; i++) {
