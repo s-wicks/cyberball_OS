c6f08fd46845ea2af2b1487bd8458972b951bb84
Landon Holland <landon.holland02@gmail.com>
2024-04-17 16:39:40 -0400

    fixed css spacing for player

--- src/pages/Home/home.ts
+++ src/pages/Home/home.ts
@@ -160,6 +160,8 @@ export class HomeViewModel {
             this.settings.player.portraitBuff = reader.result as ArrayBuffer;
             this.updateUrl();
         };
+        const img = document.getElementById('player-portrait-img') as HTMLImageElement;
+        img.style.display = 'inline';
     }
     cpuFileSelected(cpu:CpuSettingsModel, e:any) {
         console.log(cpu);
